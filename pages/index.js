a1bd9cb8e32ea130df55f9ca913cce2b8baef0df
gloria-tampuri <gloriatampuri15@gmail.com>
2022-09-22 12:08:48 +0000

    contentful

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import {createClient} from 'contentful'
 import Hero from '../components/Hero/Hero'
 import Layout from '../components/Layout/Layout'
 import Products from '../components/Products/Products'
 
 
-const Index = () => {
+const Index = (props) => {
+
+  console.log(props.products);
+
   return (
  <Layout>
   <Hero/>
@@ -14,3 +18,20 @@ const Index = () => {
 }
 
 export default Index
+
+
+export async function getStaticProps() {
+
+  const client = createClient({
+    space:process.env.CONTENTFUL_SPACE_ID ,
+    accessToken:process.env.CONTENTFUL_ACCESS_TOKEN
+  })
+
+  const response = await client.getEntries({
+    content_type:'products'
+  })
+
+  return {
+    props: {products: response.items, revalidate: 1}
+  }
+}
