783fc6348f6ec9ec449021f866084a359f67788f
gloria-tampuri <gloriatampuri15@gmail.com>
2022-10-13 13:01:00 +0000

    deleted category api

--- pages/api/category.js
+++
@@ -1,7 +0,0 @@
-import { getProducts } from "../../Helpers/queries"
-
-export  default async function handler(req, res) {
-    const products = await getProducts()
-    res.status(200).json({ products })
-  }
-  
