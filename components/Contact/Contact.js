6c68cba6ddf190f8d3609887bf7b07c14d7fcde2
gloria-tampuri <gloriatampuri15@gmail.com>
2022-12-08 16:11:51 +0000

    email contact

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import classes from './Contact.module.css'
 import Layout from '../Layout/Layout'
 import Link from 'next/link'
 import Rotate from 'react-reveal/Rotate';
+import emailjs from '@emailjs/browser';
 import { MdEmail } from 'react-icons/md'
 import { IoIosCall } from 'react-icons/io'
 import { IoLocationSharp } from 'react-icons/io5'
@@ -11,6 +12,54 @@ import { TbBrandTwitter } from 'react-icons/tb'
 import { TbBrandInstagram } from 'react-icons/tb'
 
 const Contact = () => {
+    const form = useRef();
+ const showEnv = process.env.NEXT_PUBLIC_SERVICE_ID
+    // console.log(typeof`${showEnv}`);
+
+    const[firstName, setFirstName]=useState('')
+    const[lastName, setLastname]= useState('')
+    const[email, setEmail]=useState('')
+    const[phone, setPhone] =useState('')
+    const [message, setMessage]= useState('')
+
+    const onFormSubmit=(e)=>{
+        e.preventDefault()
+
+        emailjs.sendForm(process.env.NEXT_PUBLIC_SERVICE_ID,process.env.NEXT_PUBLIC_TEMPLATE_ID, form.current, process.env.NEXT_PUBLIC_PUBLIC_KEY)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+        const recievedMsg={
+            firstName,
+            lastName,
+            email,
+            phone,
+            message
+        }
+        console.log(recievedMsg);
+
+        setEmail('')
+        setFirstName('')
+        setLastname('')
+        setMessage('')
+        setPhone('')
+    }
+
+    // const sendEmail = (e) => {
+    //     e.preventDefault();
+    //     const serviceId=process.env.SERVICE_ID
+
+    //     emailjs.sendForm("service_niyip6b","template_3m65f2b", form.current,"jk3fznquANl-FgKAE")
+    //         .then((result) => {
+    //             console.log(result.text);
+    //         }, (error) => {
+    //             console.log(error.text);
+    //         });
+    // };
+
     return (
         <Layout>
 
@@ -21,79 +70,89 @@ const Contact = () => {
 
             <div className={classes.mainlayout}>
 
-              <Rotate left>
-              <div className={classes.ourcontact}>
-                    <h2>
-                        Contact Information
-                    </h2>
-                    <p> Fill up the form and our team will get back to you within 24 hours</p>
+                <Rotate left>
+                    <div className={classes.ourcontact}>
+                        <h2>
+                            Contact Information
+                        </h2>
+                        <p> Fill up the form and our team will get back to you within 24 hours</p>
 
-                    <div className={classes.contactsection}>
-                        <IoIosCall className={classes.socialicon} />
-                        <Link href="tel:+233595850394"><a href="tel:+233595850394">+233595850394 / 0302243847</a></Link>
-                    </div>
+                        <div className={classes.contactsection}>
+                            <IoIosCall className={classes.socialicon} />
+                            <Link href="tel:+233595850394"><a href="tel:+233595850394">+233595850394 / 0302243847</a></Link>
+                        </div>
 
-                    <div className={classes.contactsection}>
-                        <IoIosCall className={classes.socialicon} />
-                        <Link href="tel:+233508737663"><a href="tel:+233508737663">+233508737663 (Kumasi)</a></Link>
-                    </div>
+                        <div className={classes.contactsection}>
+                            <IoIosCall className={classes.socialicon} />
+                            <Link href="tel:+233508737663"><a href="tel:+233508737663">+233508737663 (Kumasi)</a></Link>
+                        </div>
 
-                    <div className={classes.contactsection}>
-                        <MdEmail className={classes.socialicon} />
-                        <Link href='mailto:info@gabnetprinting.com'><a href='mailto:info@gabnetprinting.com'>info@gabnetprinting.com</a></Link>
-                    </div>
+                        <div className={classes.contactsection}>
+                            <MdEmail className={classes.socialicon} />
+                            <Link href='mailto:info@gabnetprinting.com'><a href='mailto:info@gabnetprinting.com'>info@gabnetprinting.com</a></Link>
+                        </div>
 
-                    <div className={classes.contactsection}>
-                        <IoLocationSharp className={classes.location} />
-                        <Link href='https://www.google.com/maps/place/House+Party+Computers/@5.5882174,-0.2098218,17z/data=!3m1!4b1!4m5!3m4!1s0xfdf9a38d31c2bd7:0x4c44cda630d1e91e!8m2!3d5.5882174!4d-0.2076331'><a href='https://www.google.com/maps/place/House+Party+Computers/@5.5882174,-0.2098218,17z/data=!3m1!4b1!4m5!3m4!1s0xfdf9a38d31c2bd7:0x4c44cda630d1e91e!8m2!3d5.5882174!4d-0.2076331'>House Party Main Building,opposite Republic and NIB Banks, Accra-Ghana / Almadiya Roundabout, Kumasi</a></Link>
-                    </div>
+                        <div className={classes.contactsection}>
+                            <IoLocationSharp className={classes.location} />
+                            <Link href='https://www.google.com/maps/place/House+Party+Computers/@5.5882174,-0.2098218,17z/data=!3m1!4b1!4m5!3m4!1s0xfdf9a38d31c2bd7:0x4c44cda630d1e91e!8m2!3d5.5882174!4d-0.2076331'><a href='https://www.google.com/maps/place/House+Party+Computers/@5.5882174,-0.2098218,17z/data=!3m1!4b1!4m5!3m4!1s0xfdf9a38d31c2bd7:0x4c44cda630d1e91e!8m2!3d5.5882174!4d-0.2076331'>House Party Main Building,opposite Republic and NIB Banks, Accra-Ghana / Almadiya Roundabout, Kumasi</a></Link>
+                        </div>
 
-                    {/* <div className={socials}>
+                        {/* <div className={socials}>
                     <img src="/svgfiles/facebook.svg" alt="facebook">
                     <img src="/svgfiles/twitter.svg" alt="twitter">
                     <img src="/svgfiles/instagram.svg" alt="instagram">
                 </div> */}
 
-                    <div className={classes.socials}>
-                        <FiFacebook className={classes.socialmedia} />
-                        <TbBrandTwitter className={classes.socialmedia} />
-                        <TbBrandInstagram className={classes.socialmedia} />
-                    </div>
+                        <div className={classes.socials}>
+                            <FiFacebook className={classes.socialmedia} />
+                            <TbBrandTwitter className={classes.socialmedia} />
+                            <TbBrandInstagram className={classes.socialmedia} />
+                        </div>
 
-                    <div className={classes.cone}>
+                        <div className={classes.cone}>
+                        </div>
+                        <div className={classes.circle}>
+                        </div>
                     </div>
-                    <div className={classes.circle}>
-                    </div>
-                </div>
-              </Rotate>
+                </Rotate>
 
                 <div className={classes.yourcontact}>
-                    <form action="" className={classes.form}>
+                    <form ref={form} className={classes.form} onSubmit={onFormSubmit}>
                         <div className={classes.formsection}>
                             <label htmlFor="first-name">First Name</label>
-                            <input type="text" name="first-name" id="first-name" placeholder="John" />
+                            <input type="text" name="first-name" id="first-name" placeholder="John" required value={firstName} onChange={(e)=>{setFirstName(e.target.value)}}/>
                         </div>
 
                         <div className={classes.formsection}>
                             <label htmlFor="last-name">Last Name</label>
-                            <input type="text" name="last-name" id="last-name" placeholder="Doe" />
+                            <input type="text" name="last-name" id="last-name" placeholder="Doe" required value={lastName} onChange={(e)=>{setLastname(e.target.value)}} />
                         </div>
 
                         <div className={classes.formsection}>
                             <label htmlFor="mail">Mail</label>
-                            <input type="email" name="mail" id="mail" placeholder="Enter email" />
+                            <input type="email" name="mail" id="mail" placeholder="Enter email" value={email} onChange={(e)=>{setEmail(e.target.value)}} />
                         </div>
 
                         <div className={classes.formsection}>
                             <label htmlFor="phone">Phone</label>
-                            <input type="tel" name="phone" id="phone" placeholder="Enter phone number" />
+                            <input type="tel" name="phone" id="phone" placeholder="Enter phone number" value={phone} required onChange={(e)=>{setPhone(e.target.value)}} />
                         </div>
                         <div className={`${classes.formsection} ${classes.textarea}`}>
                             <label htmlFor="message"> Message</label>
-                            <textarea name="" id="" cols="30" rows="10"></textarea>
+                            <textarea name="message" id="" cols="30" rows="10" value={message} onChange={(e)=>{setMessage(e.target.value)}}></textarea>
                         </div>
                         <button className={classes.button}>Send Message</button>
                     </form>
+
+                    {/* <form ref={form} onSubmit={sendEmail}>
+                        <label>Name</label>
+                        <input type="text" name="user_name" />
+                        <label>Email</label>
+                        <input type="email" name="user_email" />
+                        <label>Message</label>
+                        <textarea name="message" />
+                        <input type="submit" value="Send" />
+                    </form> */}
 
                 </div>
 
