3c8e9efd8d173f1205ce188a8cb281cf89c3eaf1
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2024-02-02 13:46:15 +0000

    updated footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -19,25 +19,6 @@ const Footer = () => {
         <hr />
 
         <div className={classes.biggercolumns}>
-
-          <div className={classes.rows}>
-            <ul>
-              <h4>Abous Us</h4>
-              {/* <li>Gabnet Enterprise is a subsidiary</li>
-                <li> of House Party Computers.</li>
-                <li> We deal in the sale of quality</li>
-                <li>Embroidery Machines, Large Format </li>
-                <li> Printing Machines, New/Refurbished</li>
-                <li> printers/photocopiers</li> */}
-              <li className={classes.aboutUs}>Gabnet Enterprise aims at providing our customers with VALUE FOR MONEY deals on   Printers, Copiers and consumables for their printing requirements. We stock a wide range of printing equipment such as Heat press, screen printing, digital embroidery, plasma, fiber laser, DTF printers, roll laminators, CO2 laser and many more.<br></br>
-                We adapt an honest and sincere approach to doing business with our customers and we are passionate about supporting small printing shops to grow with and make more profit. <br></br>
-                Over the years we have supported small printing businesses to grow their sales and profit by providing them with technical support, appropriate equipment and bringing new technology to their door step at affordable prices.
-                We are based in Accra, with a branch in Kumasi in the Ashanti Region.
-                We have thousands of satisfied customers across Ghana including Small & medium printing businesses, Hotels and schools.
-              </li>
-            </ul>
-          </div>
-
           <div className={classes.rows}>
             <ul>
               <h4>Ways to buy</h4>
