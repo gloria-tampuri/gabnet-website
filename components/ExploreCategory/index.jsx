c47e74ccf41b34c4f36c821d8392fa04e7283e56
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2024-02-12 14:08:46 +0000

    updated explore category

--- components/ExploreCategory/index.jsx
+++ components/ExploreCategory/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from './ExploreCategory.module.css'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const CategoryData = [
     {
@@ -16,22 +17,26 @@ const CategoryData = [
     {
         name: "Refurbished Printers",
         slug: "refurbished-printers",
-        image: '/images/gapnet-2.jpeg'
+        image: '/images/gapnet-2.jpeg',
+        path: '/printers'
     },
     {
         name: "Printing Finishing",
         slug: "printing -finishing",
-        image: '/images/gapnet-8.jpeg'
+        image: '/images/gapnet-8.jpeg',
+        path: '/printers'
     },
     {
         name: "Industrial Printers Special Commercial Machines",
         slug: "printing -finishing",
-        image: '/images/gapnet-8.jpeg'
+        image: '/images/gapnet-8.jpeg',
+        path: '/printers'
     },
     {
         name: "Printer Spare Parts",
         slug: "printer-spare-parts",
-        image: '/images/gapnet-8.jpeg'
+        image: '/images/gapnet-8.jpeg',
+        path: '/printers'
     },
 ]
 
@@ -40,10 +45,10 @@ export default function ExploreCategory() {
     return (
         <section className={classes.ExploreCategory}>
             {CategoryData.map((category) => (
-                <div className={classes.CategoryItem} key={category.slug}>
+                <Link href={""} className={classes.CategoryItem} key={category.slug}>
                     <Image width={200} height={200} src={category.image} alt={category.name} />
                     <p>{category.name}</p>
-                </div>
+                </Link>
             ))}
         </section>
     )
