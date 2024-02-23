6d66c6e3c64c3105efc692f891c4ff79103a5039
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2024-02-23 15:52:44 +0000

    fixed alert error

--- components/Carts/index.jsx
+++ components/Carts/index.jsx
@@ -2,6 +2,7 @@ import React, { useState, useRef } from 'react'
 import { useOrderData } from '../../library/jotai-config/order'
 import Image from 'next/image';
 import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { useRouter } from 'next/router';
 import emailjs from '@emailjs/browser';
 
@@ -51,7 +52,7 @@ export default function Carts() {
           hideProgressBar: false,
           closeOnClick: true,
         });
-    // Clear fields
+        // Clear fields
         setFirstName('');
         setContact('')
         setEmail('')
@@ -60,7 +61,8 @@ export default function Carts() {
         return router.push('/')
       }, function (error) {
         console.log('FAILED...', error);
-        toast.error("Something went wrong", {
+        setIsLoading(false)
+        return toast.error("Something went wrong", {
           position: "top-center",
           autoClose: 2000,
           hideProgressBar: false,
@@ -104,78 +106,82 @@ export default function Carts() {
     );
   }
   return (
-    <section className='min-h-screen'>
-      <div className='md:flex gap-4 rounded-md max-w-[1200px] mx-auto mt-1 p-4'>
-        <div className='w-full bg-white rounded min-h-20'>{element}</div>
-        {orderData.length > 0 && (
-          <div className='bg-gray-300 mt-4 md:mt-0 w-full md:w-1/3  rounded p-4 flex items-center justify-center'>
-            <form ref={form} onSubmit={sendEmail} className='w-full flex flex-col gap-2'>
-              <div className='flex flex-col'>
-                <label className='' htmlFor='firstName'>
-                  First Name
-                </label>
-                <input
-                  aria-disabled={isLoading}
-                  value={firstName}
-                  required
-                  onChange={(e) => setFirstName(e.target.value)}
-                  className='px-1 py-2 rounded outline-1 outline-neutral-500'
-                  type='text'
-                  id='firstName'
-                />
-              </div>
-              <div className='flex flex-col'>
-                <label className='' htmlFor='lastName'>
-                  Last Name
-                </label>
-                <input
-                  aria-disabled={isLoading}
-                  value={lastName}
-                  required
-                  onChange={(e) => setLastName(e.target.value)}
-                  className='px-1 py-2 rounded outline-1 outline-neutral-500'
-                  type='text'
-                  id='lastName'
-                />
-              </div>
-              <div className='flex flex-col'>
-                <label className='' htmlFor='phone'>
-                  Contact Number
-                </label>
-                <input
-                  value={contact}
-                  name='phone'
-                  aria-disabled={isLoading}
-                  required
-                  onChange={(e) => setContact(e.target.value)}
-                  className='px-1 py-2 rounded outline-1 outline-neutral-500'
-                  type='text'
-                  id='phone'
-                />
-              </div>
-              <div className='flex flex-col'>
-                <label className='' htmlFor='email'>
-                  Email
-                </label>
-                <input
-                  value={email}
-                  name='mail'
-                  aria-disabled={isLoading}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className='px-1 py-2 rounded outline-1 outline-neutral-500'
-                  type='email'
-                  required
-                  id='email'
-                />
-              </div>
-              <div className='w-full bg-green-700 rounded text-white font-semibold flex items-center justify-center my-2 p-2'>
-                <button className='w-full'>Send Order</button>
-              </div>
-            </form>
-          </div>
-        )}
-      </div>
+
+    <>
       <ToastContainer />
-    </section>
+      <section className='min-h-screen'>
+        <div className='md:flex gap-4 rounded-md max-w-[1200px] mx-auto mt-1 p-4'>
+          <div className='w-full bg-white rounded min-h-20'>{element}</div>
+          {orderData.length > 0 && (
+            <div className='bg-gray-300 mt-4 md:mt-0 w-full md:w-1/3  rounded p-4 flex items-center justify-center'>
+              <form ref={form} onSubmit={sendEmail} className='w-full flex flex-col gap-2'>
+                <div className='flex flex-col'>
+                  <label className='' htmlFor='firstName'>
+                    First Name
+                  </label>
+                  <input
+                    aria-disabled={isLoading}
+                    value={firstName}
+                    required
+                    onChange={(e) => setFirstName(e.target.value)}
+                    className='px-1 py-2 rounded outline-1 outline-neutral-500'
+                    type='text'
+                    id='firstName'
+                  />
+                </div>
+                <div className='flex flex-col'>
+                  <label className='' htmlFor='lastName'>
+                    Last Name
+                  </label>
+                  <input
+                    aria-disabled={isLoading}
+                    value={lastName}
+                    required
+                    onChange={(e) => setLastName(e.target.value)}
+                    className='px-1 py-2 rounded outline-1 outline-neutral-500'
+                    type='text'
+                    id='lastName'
+                  />
+                </div>
+                <div className='flex flex-col'>
+                  <label className='' htmlFor='phone'>
+                    Contact Number
+                  </label>
+                  <input
+                    value={contact}
+                    name='phone'
+                    aria-disabled={isLoading}
+                    required
+                    onChange={(e) => setContact(e.target.value)}
+                    className='px-1 py-2 rounded outline-1 outline-neutral-500'
+                    type='text'
+                    id='phone'
+                  />
+                </div>
+                <div className='flex flex-col'>
+                  <label className='' htmlFor='email'>
+                    Email
+                  </label>
+                  <input
+                    value={email}
+                    name='mail'
+                    aria-disabled={isLoading}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className='px-1 py-2 rounded outline-1 outline-neutral-500'
+                    type='email'
+                    required
+                    id='email'
+                  />
+                </div>
+                <div className={`w-full ${isLoading ? 'cursor-not-allowed' : ''} ${isLoading ? 'bg-gray-600' : 'bg-green-600'} rounded text-white font-semibold flex items-center justify-center my-2 p-2`}>
+                  <button disabled={isLoading} className='w-full'>{isLoading ? 'Submitting...' : 'Submit'}</button>
+                </div>
+              </form>
+            </div>
+          )}
+        </div>
+      </section>
+    </>
+
   );
 }
